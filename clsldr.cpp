14af9bfe0177e5b6ca730f3dbc8a4f9d253a4b7a
gau2112 <gautam.gaya123@gmail.com>
2017-03-16 11:24:45 +0530

    Playgame-Playgame

---
+++ clsldr.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int call(int n,int st,int jump)
+{
+
+}
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+        int n,l,jump;
+        cin>>n>>l>>jump;
+        int val = call(n,l,jump);
+	}
+}
---
+++ playgame.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long long int t,n;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		if(n%3==0)
+			cout<<"Ragini"<<endl;
+		else
+			cout<<"Hemlata"<<endl;
+	}
+	return 0;
+}
